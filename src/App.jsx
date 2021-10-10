eb4397ac072d3ae7b066eb4976cc88c4c0956531
Thanathip Suwannakhot <dannajaeiei@hotmail.com>
2021-10-11 00:30:43 +0700

    add background image and color for each activity

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,25 @@ import { MoreHorizOutlined } from "@material-ui/icons";
 import data from "./data.json";
 
 const App = () => {
+  const checkClass = (title) => {
+    switch (title) {
+      case "Work":
+        return "activity work";
+      case "Play":
+        return "activity play";
+      case "Study":
+        return "activity study";
+      case "Exercise":
+        return "activity exercise";
+      case "Social":
+        return "activity social";
+      case "Self Care":
+        return "activity selfCare";
+      default:
+        break;
+    }
+  }
+  
   return (
     <div className="app">
       <div className="wrapper">
@@ -12,22 +31,17 @@ const App = () => {
         </div> 
         <div className="sub-main">
           {data.map((item) => (
-            <div className="activity">
+            <div className={checkClass(item.title)}>
               <div className="textContent">
                 <div className="top">
                   <span>{item.title}</span>
-                  <MoreHorizOutlined />
+                  <MoreHorizOutlined style={{ cursor: "pointer" }}/>
                 </div>
                 <h1>{item.timeframes.weekly.current}hrs</h1> 
                 <span>Last week - {item.timeframes.weekly.previous}hrs</span> 
               </div>
             </div>
           ))}
-          {/* <div className="activity"></div>
-          <div className="activity"></div>
-          <div className="activity"></div>
-          <div className="activity"></div>
-          <div className="activity"></div> */}
         </div>
       </div>
     </div>
