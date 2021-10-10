0ebd3c8ffc8792e2052a3a1d489de86206cd4417
Thanathip Suwannakhot <dannajaeiei@hotmail.com>
2021-10-10 22:39:28 +0700

    create flexbox for each div

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,20 @@ import "./app.scss";
 const App = () => {
   return (
     <div className="app">
-
+      <div className="wrapper">
+        <div className="main">
+          <div className="profile"></div>  
+          <div className="period"></div>  
+        </div> 
+        <div className="sub-main">
+          <div className="activity"></div>
+          <div className="activity"></div>
+          <div className="activity"></div>
+          <div className="activity"></div>
+          <div className="activity"></div>
+          <div className="activity"></div>
+        </div>
+      </div>
     </div>
   )
 }
