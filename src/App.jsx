c190833110392b3d3696c8dc1f1637b1f64236b4
Thanathip Suwannakhot <dannajaeiei@hotmail.com>
2021-10-10 23:30:02 +0700

    add weekly content to each box

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,6 @@
 import "./app.scss";
+import { MoreHorizOutlined } from "@material-ui/icons";
+import data from "./data.json";
 
 const App = () => {
   return (
@@ -9,12 +11,23 @@ const App = () => {
           <div className="period"></div>  
         </div> 
         <div className="sub-main">
+          {data.map((item) => (
+            <div className="activity">
+              <div className="textContent">
+                <div className="top">
+                  <span>{item.title}</span>
+                  <MoreHorizOutlined />
+                </div>
+                <h1>{item.timeframes.weekly.current}hrs</h1> 
+                <span>Last week - {item.timeframes.weekly.previous}hrs</span> 
+              </div>
+            </div>
+          ))}
+          {/* <div className="activity"></div>
           <div className="activity"></div>
           <div className="activity"></div>
           <div className="activity"></div>
-          <div className="activity"></div>
-          <div className="activity"></div>
-          <div className="activity"></div>
+          <div className="activity"></div> */}
         </div>
       </div>
     </div>
